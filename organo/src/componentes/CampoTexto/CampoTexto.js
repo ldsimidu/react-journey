e8c719e81f9275edce76ce3012ad6be295e0ac7e
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-04 23:46:28 -0300

    FEAT: Adding "CampoTexto" structure

---
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,13 @@
+export const CampoTexto = (props) => {
+    return (
+        <div className="container pt-5 p-4 bg-light shadow-sm">
+            <label htmlFor="nome" className="form-label">{props.label}</label>
+            <input 
+                type="text" 
+                className="form-control" 
+                id="nome" 
+                placeholder="Digite seu nome" 
+            />
+        </div>
+    )
+}
