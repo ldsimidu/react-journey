254771c9f7a568e4b358215dba44059594db383b
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-07 00:04:58 -0300

    CHORE: Changing "ColaboradorForms" to "Formulario"

---
+++ organo/src/componentes/Formulario/Formulario.js
@@ -0,0 +1,31 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
+
+export const Formulario = () => {
+
+    const times = [
+        'G2',
+        'MIBR',
+        'LOUD',
+        '2Game',
+        'Sentinels',
+        'Furia',
+        '100Thieves'
+    ]
+
+    return (
+        <div className="formulario__container container mt-5 mb-5 rounded-p">
+            <h2 className="formulario__label-title pt-5 mb-4">Preencha os dados para criar o card do colaborador</h2>
+            <form>
+                <CampoTexto label='Nome' placeholder='Digite seu nome'/>
+                <CampoTexto label='Cargo' placeholder='Digite seu cargo'/>
+                <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem'/>
+
+                <ListaSuspensa itens={times} />
+
+                <button type="submit" className="mb-4 btn btn-primary mt-3">Criar card</button>
+            </form>
+        </div>
+  );
+}
