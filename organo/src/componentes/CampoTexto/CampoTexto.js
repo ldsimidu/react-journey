f35397b779a09cfb09226650640fc88db5f9ef66
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-07 00:04:03 -0300

    STYLE: Changing CSS according to the Formulario section on FIGMA project

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -1,14 +1,17 @@
+import './CampoTexto.css'
+
 export const CampoTexto = (props) => {
     return (
-        <div className="container pt-5 p-4 bg-light shadow-sm">
-            <label htmlFor="nome" className="form-label">
+        <div className="CampoTexto__container container pt-5 p-4">
+            <label htmlFor="nome" className="CampoTextoLabel form-label">
                 {props.label}
             </label>
-            <input 
+            <input
                 type="text" 
-                className="form-control" 
+                className="CampoText__input form-control border-0" 
                 id="nome" 
-                placeholder={`Digite seu ${props.label}`}
+                //placeholder={`Digite seu ${props.label}`}
+                placeholder={props.placeholder}
             />
         </div>
     )
