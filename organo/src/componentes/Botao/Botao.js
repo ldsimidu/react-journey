7ef15f1f61ce7635561dd80e62677680c70bf382
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-07 01:35:22 -0300

    STYLE: Changing content to children + styling button hover

--- organo/src/componentes/Botao/Botao.js
+++ organo/src/componentes/Botao/Botao.js
@@ -4,8 +4,12 @@ export const Botao = (props) => {
     return (
         <div className="container">
             <button className="Botao__button mb-5">
-                {props.texto}
+                {props.children}
             </button>
         </div>
     )
 }
+
+//children utilizado na linha 7, pois no botão pode ser inserido
+//qualquer tipo de dado, seja imagem etc, então é mais prático
+//puxar o conteúdo dentro de Botao em Formulario com o children
