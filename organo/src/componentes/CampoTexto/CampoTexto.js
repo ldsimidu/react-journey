22c8a46332f8051e54f387f36b9080f41c32f744
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-08 00:20:28 -0300

    FEAT: Change CampoTexto to a controlled component (incomplete)

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -1,12 +1,22 @@
 import './CampoTexto.css'
 
 export const CampoTexto = (props) => {
+
+    let valor = 'Lucas Derenze SImidu'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="CampoTexto__container container">
             <label htmlFor="nome" className="CampoTexto__label form-label">
                 {props.label}
             </label>
             <input
+                value={valor}
+                onChange={aoDigitado}
                 required={props.obrigatorio}
                 type="text" 
                 className="CampoText__input form-control border-0" 
