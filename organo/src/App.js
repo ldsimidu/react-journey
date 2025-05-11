842522314c7e6f77fa6fcbe33ca07e4d551839e8
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-11 02:31:13 -0300

    FEAT: Adding V01 "Time" component

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,12 +1,17 @@
 import './App.css'
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() { //componente react
   return (
     <div className="App">
       <Banner/>
       <Formulario/>
+      <Time nome="G2"/>
+      <Time nome="LOUD"/>
+      <Time nome="MIBR"/>
+      <Time nome="Sentinels"/>
     </div>
   );
 }
---
+++ organo/src/componentes/Time/Time.js
@@ -0,0 +1,9 @@
+import './Time.css'
+
+export const Time = (props) => {
+    return (
+        <section className='time__section'>
+            <h1>{props.nome}</h1>
+        </section>
+    )
+}
