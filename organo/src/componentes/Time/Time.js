95779a457c7c8f246586333879ec35dce86fc0ea
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-11 23:54:01 -0300

    FEAT: Changing "Time" component to consume "Colaborador""

--- organo/src/componentes/Time/Time.js
+++ organo/src/componentes/Time/Time.js
@@ -1,14 +1,17 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 export const Time = (props) => {
     
     const cSec = { backgroundColor: props.corSecundaria }
-    const cPri = { borderColor: props.corPrimaria }
+    const cPri = { backgroundColor: props.corPrimaria }
+    const cBorder = { borderColor: props.corPrimaria }
 
     return (
         <section 
             className='time__section' style={cSec}>
-            <h1 style={cPri}>{props.nome}</h1>
+            <h1 style={cBorder}>{props.nome}</h1>
+            {props.colaboradores.map(colaborador => <Colaborador cor={cPri} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
         </section>
     )
 }
