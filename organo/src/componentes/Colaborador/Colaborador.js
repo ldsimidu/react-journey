435540a68321eb3a11c260abfda0de88ddde42fa
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-15 01:43:53 -0300

    FIX: Working at "Colaborador" responsiveness + changing classNames

--- organo/src/componentes/Colaborador/Colaborador.js
+++ organo/src/componentes/Colaborador/Colaborador.js
@@ -2,11 +2,11 @@ import './Colaborador.css'
 
 export const Colaborador = ({ cor, nome, cargo, imagem}) => {
     return (
-        <div className="colaborador">
-            <div className="cabecalho" style={cor}>
-                <img className="imagem" src={imagem} alt="Foto do colaborador" />
+        <div className="colaborador__container">
+            <div className="colaborador__header" style={cor}>
+                <img className="colaborador__imagem" src={imagem} alt="Foto do colaborador" />
             </div>
-            <div className="rodape">
+            <div className="colaborador__footer">
                 <h4>{nome}</h4>
                 <h5>{cargo}</h5>
             </div>
