3ece9bb3917e7baa61aaab45ac83226aa60b94fe
Lucas Simidu <ldsimidu@gmail.com>
2026-05-03 11:54:23 -0300

    style: lint extension fixes

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -1,29 +1,34 @@
-import './CampoTexto.css'
+import CheckBoxGitHub from "../CheckBoxGitHub/CheckBox";
+import "./CampoTexto.css";
 
 export const CampoTexto = (props) => {
+  // Função que é chamada sempre que o usuário digitar no input (que possua onChange)
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+    //setValor(evento.target.value) // Atualiza o estado 'valor' com o conteúdo digitado no campo
+    //console.log(valor)
+  };
 
-    // Função que é chamada sempre que o usuário digitar no input (que possua onChange)
-    const aoDigitado = (evento) => {
-        props.aoAlterado(evento.target.value)
-        //setValor(evento.target.value) // Atualiza o estado 'valor' com o conteúdo digitado no campo
-        //console.log(valor)
-    }
-
-    return (
-        <div className="CampoTexto__container">
-            <label htmlFor="nome" className="CampoTexto__label form-label">
-                {props.label}
-            </label>
-            <input
-                value={props.valor}
-                onChange={aoDigitado}
-                required={props.obrigatorio}
-                type="text" 
-                className="CampoText__input form-control border-0" 
-                id="nome" 
-                //placeholder={`Digite seu ${props.label}`}
-                placeholder={props.placeholder}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="CampoTexto__container">
+      <label htmlFor="nome" className="CampoTexto__label form-label">
+        {props.label}
+      </label>
+      {props.label === "Imagem" ? (
+        <CheckBoxGitHub placeholder="Imagem do Github" />
+      ) : (
+        ""
+      )}
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        type="text"
+        className="CampoText__input form-control border-0"
+        id="nome"
+        //placeholder={`Digite seu ${props.label}`}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
--- organo/src/componentes/Formulario/Formulario.js
+++ organo/src/componentes/Formulario/Formulario.js
@@ -1,72 +1,78 @@
-import './Formulario.css'
-import CampoTexto from '../CampoTexto';
-import ListaSuspensa from '../ListaSuspensa';
-import Botao from '../Botao';
-import { useState } from 'react';
+import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
+import { useState } from "react";
+import CheckBoxGitHub from "../CheckBoxGitHub/CheckBox";
 
 export const Formulario = (props) => {
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
 
-    const [nome, setNome] = useState('')
-    const [cargo, setCargo] = useState('')
-    const [imagem, setImagem] = useState('')
-    const [time, setTime] = useState('')
+  const aoSalvar = (evento) => {
+    evento.preventDefault(); //evento que impede auto carregar do botão
+    props.novoColaborador({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
+    setNome("");
+    setCargo("");
+    setImagem("");
+    setTime("");
 
-    const aoSalvar = (evento) => {
-        evento.preventDefault() //evento que impede auto carregar do botão
-        props.novoColaborador({
-            nome,
-            cargo,
-            imagem,
-            time
-        })
-        setNome('')
-        setCargo('')
-        setImagem('')
-        setTime('')
+    console.log(
+      "Form foi submetido\n------------------------\n",
+      "\nNome:",
+      nome,
+      "\nCargo:",
+      cargo,
+      "\nImagem:",
+      imagem,
+      "\nTime:",
+      time,
+    );
+  };
 
-        console.log('Form foi submetido\n------------------------\n',
-            '\nNome:', nome, 
-            '\nCargo:', cargo, 
-            '\nImagem:', imagem,
-            '\nTime:', time
-        )
-    }
+  return (
+    <div className="formulario__container container">
+      <h2 className="formulario__label-title">
+        Preencha os dados para criar o card do colaborador
+      </h2>
+      <form onSubmit={aoSalvar}>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Imagem"
+          placeholder="Informe o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
 
-    return (
-        <div className="formulario__container container">
-            <h2 className="formulario__label-title">Preencha os dados para criar o card do colaborador</h2>
-            <form onSubmit={aoSalvar}>
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label='Nome' 
-                    placeholder='Digite seu nome'
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}
-                />
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label='Cargo' 
-                    placeholder='Digite seu cargo'
-                    valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}
-                />
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label='Imagem' 
-                    placeholder='Informe o endereço da imagem'
-                    valor={imagem}
-                    aoAlterado={valor => setImagem(valor)}
-                />
-                <ListaSuspensa 
-                    itens={props.times} 
-                    obrigatorio={true}
-                    valor={time}
-                    aoAlterado={valor => setTime(valor)}    
-                />
-                <Botao>
-                    Criar Card
-                </Botao>
-            </form>
-        </div>
+        <ListaSuspensa
+          itens={props.times}
+          obrigatorio={true}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
+        <Botao>Criar Card</Botao>
+      </form>
+    </div>
   );
-}
+};
