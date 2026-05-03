5be11cc0c7c2ad2d25707e43b88350b98ed09aa0
Lucas Simidu <ldsimidu@gmail.com>
2026-05-03 15:33:37 -0300

    style: Lint style app.js

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,75 +1,80 @@
-import { useState } from 'react';
-import './App.css'
-import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulario';
-import Titulo from './componentes/Titulo';
-import Time from './componentes/Time';
-import Footer from './componentes/Footer';
+import { useState } from "react";
+import "./App.css";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Titulo from "./componentes/Titulo";
+import Time from "./componentes/Time";
+import Footer from "./componentes/Footer";
 
-function App() { //componente react
+function App() {
+  //componente react
 
   const times = [
     {
-      nome: 'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
-    }, 
-    {
-      nome: 'Front End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      nome: "Programação",
+      corPrimaria: "#57C278",
+      corSecundaria: "#D9F7E9",
     },
     {
-      nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      nome: "Front End",
+      corPrimaria: "#82CFFA",
+      corSecundaria: "#E8F8FF",
     },
     {
-      nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      nome: "Data Science",
+      corPrimaria: "#A6D157",
+      corSecundaria: "#F0F8E2",
     },
     {
-      nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      nome: "Devops",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#FDE7E8",
     },
     {
-      nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      nome: "UX e Design",
+      corPrimaria: "#DB6EBF",
+      corSecundaria: "#FAE9F5",
     },
     {
-      nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
-    }
-  ]
+      nome: "Mobile",
+      corPrimaria: "#FFBA05",
+      corSecundaria: "#FFF5D9",
+    },
+    {
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8A29",
+      corSecundaria: "#FFEEDF",
+    },
+  ];
 
-  const [colaboradores, setColaboradores] = useState([]) //criando uma lista que será armazenada os novos Colaboradores
+  const [colaboradores, setColaboradores] = useState([]); //criando uma lista que será armazenada os novos Colaboradores
 
-  const newColaborador = (colaborador) => { //Criando função para novoColaborador
-    console.log(colaborador)
-    setColaboradores([...colaboradores, colaborador]) //essas reticências evita o bug de criar 2 cards de uma vez, espalhas os itens no array
-  }
+  const newColaborador = (colaborador) => {
+    //Criando função para novoColaborador
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]); //essas reticências evita o bug de criar 2 cards de uma vez, espalhas os itens no array
+  };
 
   return (
     <div className="App">
-      <Banner/>
-      <Formulario 
-        times={times.map(time => time.nome)} 
-        novoColaborador={colaborador => newColaborador(colaborador)}
+      <Banner />
+      <Formulario
+        times={times.map((time) => time.nome)}
+        novoColaborador={(colaborador) => newColaborador(colaborador)}
       />
-      <Titulo colaboradores={colaboradores}/>
-      {times.map(time => 
-        <Time 
-          key={time.nome} 
-          nome={time.nome} 
-          corPrimaria={time.corPrimaria} 
+      <Titulo colaboradores={colaboradores} />
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria}
-          colaboradores={colaboradores.filter(colaborador => colaborador.time == time.nome)}
-        />)} 
-        <Footer/>
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.time == time.nome,
+          )}
+        />
+      ))}
+      <Footer />
     </div>
   );
 }
